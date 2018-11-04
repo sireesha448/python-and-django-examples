c8daaf4c2153292f8705b27b767ad0488a5c2605
sireesha448 <44664608+sireesha448@users.noreply.github.com>
2018-11-04 15:46:33 +0530

    examples

---
+++ conditional control/demo11.py
@@ -0,0 +1,7 @@
+tmarks=int(input("emter marks:"))
+if tmarks>360:
+    print("First class")
+elif tmarks>=300 and tmarks<360:
+    print("second class")
+else:
+    print("third class")
---
+++ loops/demo27.py
@@ -0,0 +1,11 @@
+big1=0
+big2=0
+for x in range(1,11):
+    no1=int(input("enter number:"))
+    if big1<no1:
+        if big2<no1:
+            big2=big1
+            big1=no1
+
+print(big1,"is a 1st big number")
+print(big2,"is a 2nd big number ")
