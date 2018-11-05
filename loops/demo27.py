dd88e75004e62df8a901d5d4e881ac4d24658dfd
sireesha448 <44664608+sireesha448@users.noreply.github.com>
2018-11-05 13:49:41 +0530

    example

--- loops/demo27.py
+++ loops/demo27.py
@@ -7,5 +7,9 @@ for x in range(1,11):
             big2=big1
             big1=no1
 
+    else:
+        if big2<no1:
+            big2=no1
+
 print(big1,"is a 1st big number")
 print(big2,"is a 2nd big number ")
